c9c4aef8ecca3a482f4b9c96d8f2c6852e0fc438
Oksana Bershadska <ksenaua@icloud.com>
2025-10-09 01:18:32 +0200

    Final version for HW4

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { MovieResponse } from "../types/movie";
 
-const API_KEY = "Y374cc60661f442e0205aec739b89ac8f";
+const API_KEY = "374cc60661f442e0205aec739b89ac8f";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const fetchMovies = async (
